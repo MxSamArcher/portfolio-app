fbd1d950366680af0496c3cb45c5529f11551905
Sam Archer <mx.sam.archer@gmail.com>
2024-04-11 10:25:32 +1000

    Managed to get a response from GitHub with public repo info. Next step: Refine the data and display it on the page.

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,43 @@
 import { useState } from "react";
+import { Octokit } from "octokit";
 
 export default function Home() {
+  const octokit = new Octokit({ 
+    auth: process.env.NEXT_PUBLIC_REACT_APP_GITHUB_API_KEY !== false ? 
+    process.env.NEXT_PUBLIC_REACT_APP_GITHUB_API_KEY
+    : process.env.GITHUB_API_KEY
+  });
   
-  const fetchProjects = async (url_id) => {
+  const fetchProjects = async () => {
+
     try {
+      // 'REST API Endpoints for xyz' is what you want
+      const response = await octokit.request('GET /users/{username}/repos', {
+        username: 'MxSamArcher',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+      console.log(response)
       //fetch request for projects array. The fetch should retrieve all the public projects for my account
     } catch(error) {
       console.log(`Error: ${error}`)
     }
   }
 
+  fetchProjects();
+
   return (
     <div className='flex flex-col justify-left w-full h-full min-h-screen text-gray-800 bg-sky-100'>
-      <header className='bg-sky-900 text-white p-6 shadow-md'>
+      <header className=' p-6 shadow-md'>
         Header
       </header>
-      <div className="flex flex-grow">
-        <nav className='bg-sky-900 flex-shrink-0 p-4 min-w-[10%] text-white shadow-inner'>
+      <div className='flex flex-grow'>
+        <nav className=' flex-shrink-0 p-4 min-w-[10%]  shadow-inner'>
           Nav
         </nav>
-        <main className='bg-sky-800 flex-grow p-4 text-white'>
+        <main className='flex justify-center flex-grow p-4'>
           Main content
         </main>
       </div>
