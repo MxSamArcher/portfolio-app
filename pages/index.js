437fab7cfc2f22f9fa29caa36ccac453e409dbb4
Sam Archer <mx.sam.archer@gmail.com>
2024-04-12 14:20:29 +1000

    figuring out the principal of least privelage and separation of concerns for fetching arrays of data and iterating through that data

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,24 @@ import { useState } from "react";
 import { Octokit } from "octokit";
 
 export default function Home() {
+const [repos, setRepos] = useState([])
+
   const octokit = new Octokit({ 
-    auth: process.env.NEXT_PUBLIC_REACT_APP_GITHUB_API_KEY !== false ? 
-    process.env.NEXT_PUBLIC_REACT_APP_GITHUB_API_KEY
-    : process.env.GITHUB_API_KEY
+    auth: process.env.NEXT_PUBLIC_REACT_APP_GITHUB_API_KEY || process.env.GITHUB_API_KEY
   });
   
+  const fetchDirectoryContents = async () => {
+    try {
+      const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+        owner,
+        repo,
+        path
+      })
+    } catch {
+
+    }
+  }
+
   const fetchProjects = async () => {
 
     try {
@@ -20,7 +32,26 @@ export default function Home() {
       })
 
       console.log(response)
-      //fetch request for projects array. The fetch should retrieve all the public projects for my account
+      /*
+      The above retrieves a list of repos with key details regarding each
+      Each repo will be fetched individually using `octokit.request('GET /repos/{owner}/{repo}/contents/'`
+      The info returned from each will contain descriptive information for each file or folder in the top level of the repo, but an additional fetch will need to be performed for files in folders
+      This seems like it will need to be done recursively, but I may be wrong - there may be a way to do this en-masse without recursively fetching each set of files within a folder, but again, I may be wrong.
+
+      If the above is incorrect, the following comentary can be discregarded. If it's what needs to be done, then as far as I'm aware, my options are as follows. I could:
+      - Create a global variable that the fetch would add all the info to like an array, then map through that and create each compnent based around each of them. I don't know how I would do that store the info if I did that.
+      - Create a function inside the fetch that creates a component after each fetch to remove the need for storage.
+      */
+      const responseTwo = await octokit.request('GET /repos/{owner}/{repo}/contents/', {
+        owner: 'MxSamArcher',
+        repo: 'portfolio-app',
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28'
+        }
+      })
+
+      console.log(responseTwo)
+      
     } catch(error) {
       console.log(`Error: ${error}`)
     }
